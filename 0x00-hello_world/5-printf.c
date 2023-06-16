034be9cf33d96af6c139c16a12f0031b46bd2725
Ismail <ismailelkhalil72000@gmail.com>
2023-06-15 18:01:15 -0700

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+*main- program that prints
+*"with proper grammar, but the outcome is a piece of art,"
+*with printf function
+*Return: 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
