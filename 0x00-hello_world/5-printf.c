eb3e5b4977315457eddcc96f49c3277124392391
Ismail <ismailelkhalil72000@gmail.com>
2023-06-15 18:06:01 -0700

    Update 5-pritnf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
-*main- program that prints
-*"with proper grammar, but the outcome is a piece of art,"
-*with printf function
-*Return: 0 (success)
-*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
