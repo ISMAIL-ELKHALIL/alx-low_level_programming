3dbcb765ca0e410c0b89adb19c4228e6231a9aa1
Ismail <ismailelkhalil72000@gmail.com>
2023-06-16 18:21:17 -0700

    added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+/**
+  * main - Program to print alphabet in Lowercase in reverse from 'z' to 'a'
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char alphabet_letter;
+
+	for (alphabet_letter = 'z' ; alphabet_letter <= 'a'; alphabet_letter--)
+	putchar(alphabet_letter);
+	putchar('\n');
+	return (0);
+}
