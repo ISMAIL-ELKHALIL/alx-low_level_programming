c8c3f5ced6eadf1928511b62542ba568c5134dcb
Ismail <ismailelkhalil72000@gmail.com>
2023-06-16 19:28:26 -0700

    added 102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - Program prints all the possible combinations of  two-digit numbers
+  * numbers should range from 0 to 99
+  * numbers should be separated by a space
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+
+	int number1;
+	int number2;
+
+	for (number1 = 0; number1 < 100; number1++)
+	{
+		for (number2 = number1 + 1; number2 < 100; number2++)
+		{
+			putchar((number1 % 10) + '0');
+			putchar((number2 % 10) + '0');
+		if (number1 == 98 && number2 == 99)
+			continue;
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
