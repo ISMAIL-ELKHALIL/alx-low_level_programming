d1d59414642ad9d747c673e5b42bb1395faa2a0b
Ismail <ismailelkhalil72000@gmail.com>
2023-06-20 16:32:47 -0700

    Added 2-alphabet_x10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
   * print_alphabet - Prototype Function that prints the alphabet
   * lowercase form 'a' to 'z'.
   */
 void _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
