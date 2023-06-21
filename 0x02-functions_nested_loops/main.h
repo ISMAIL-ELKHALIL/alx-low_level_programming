c185c83240965d80ba9cf4c48fe7a5bbb4839731
Ismail <ismailelkhalil72000@gmail.com>
2023-06-20 19:00:48 -0700

    Added 9-times_table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-  * print_alphabet - Prototype Function that prints the alphabet
-  * lowercase form 'a' to 'z'.
-  */
 void _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,4 +10,5 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 #endif
