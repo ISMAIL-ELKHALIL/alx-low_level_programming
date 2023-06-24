c722fb88fe00e0e95715f7aa0eb88baf628e7b8d
Ismail <ismailelkhalil72000@gmail.com>
2023-06-23 17:03:40 -0700

    Added 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - Fonction that prints an integer.
+ * @n: The integer to be printed in the terminal.
+ */
+void print_number(int n)
+{
+	int number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	if ((number / 10) > 0)
+		print_number(number / 10);
+
+	_putchar((number % 10) + '0');
+}
