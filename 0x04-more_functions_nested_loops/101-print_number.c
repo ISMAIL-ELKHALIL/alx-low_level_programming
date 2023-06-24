4da56a2c1fea94879bae05791bb5034bed8d4f4d
Ismail <ismailelkhalil72000@gmail.com>
2023-06-23 17:15:42 -0700

    Added 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,16 @@
  */
 void print_number(int n)
 {
-	int number = n;
+	long int number = n;
 
-	if (n < 0)
+	if (number < 0)
 	{
 		_putchar('-');
 		number = -number;
 	}
-
 	if ((number / 10) > 0)
+	{
 		print_number(number / 10);
-
-	_putchar((number % 10) + '0');
+	}
+	_putchar(number % 10 + '0');
 }
