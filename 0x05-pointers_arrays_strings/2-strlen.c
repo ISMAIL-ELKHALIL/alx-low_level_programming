681b9d56e7a8cb564364ef9160788272a773c1a7
Ismail <ismailelkhalil72000@gmail.com>
2023-06-26 16:01:49 -0700

    Added 3-puts.c and update main.c ,2-strlenc

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
-  * _strlen(char *s) - Function that returns the length of a string
+  * _strlen - Function that returns the length of a string
   * @s: the string that will be tested
+  * Return: returns the length of the string s
   */
 
 int  _strlen(char *s)
 {
 	int len = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
-	return len;
+	return (len);
 }
