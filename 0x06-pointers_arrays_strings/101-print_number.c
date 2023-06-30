61e70eeaeb832a326ffb972bce024fe0baa95f4d
Ismail <ismailelkhalil72000@gmail.com>
2023-06-29 19:40:55 -0700

    added 101-print_number.c and update  main.h

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+
+void print_number(int n)
+{
+	unsigned int number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	if ((number / 10) > 0)
+		print_number(number / 10);
+	/* Print the last digit by converting it to a character and adding '0' */
+	_putchar((number % 10) + '0');
+}
