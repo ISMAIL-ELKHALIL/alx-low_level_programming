dfa2a2949b4faa8dd4b6f323e0bfc85260dd426c
Ismail <ismailelkhalil72000@gmail.com>
2023-07-03 19:14:22 -0700

    Added 1-memcpy.c and update main.h

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,8 +10,13 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
+		/* Iterate over the memory area for n iterations */
 	for (i = 0; i < n; i++)
 	{
+	/**
+	  * Assign the constant byte b to each byte
+	  * of the memory area pointed to by s
+	  */
 		s[i] = b;
 	}
 	return (s);
